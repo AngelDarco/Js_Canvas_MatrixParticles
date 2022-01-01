248a3799067397aa71a557a113e4debbbda8fd79
Darco <angeldarco>
2022-01-01 06:24:59 +0000

    V_1.0 finished

---
+++ README.md
@@ -0,0 +1,5 @@
+Image Matrix Animation, with Canvas, Only Javascript.
+
+Image Converter: https://www.base64-image.de/
+
+Page: http://darcocorporation.scienceontheweb.net/pages/javascript/particles/
--- app.js
+++ app.js
@@ -4,16 +4,16 @@ myImage.addEventListener('load', function(){
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 528;
-canvas.height = 528;
+canvas.width = 500;
+canvas.height = 700;
 
 ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height); 
 const pixels = ctx.getImageData(0,0,canvas.width,canvas.height);
-//ctx.clearReact(0,0,canvas.width,canvas.height);
-
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 let particlesArray = [];
 const numberOfParticles = 5000;
 let mappedImage = [];
+let cellBrightness;
 
 for (let y = 0; y < canvas.height; y++) {
     const row = [];
@@ -22,10 +22,9 @@ for (let x = 0; x < canvas.width; x++) {
     const green = pixels.data[(y*4*pixels.width)+(x*4+1)];
     const blue = pixels.data[(y*4*pixels.width)+(x*4+2)];
     const brightness = calculateRelativeBrightness(red, green, blue);
-    var cellBrightness;
+
     const cell = [ cellBrightness = brightness, ];
     row.push( cell);
-    
     }
     mappedImage.push(row)
 }
@@ -38,13 +37,12 @@ function calculateRelativeBrightness(red,green,blue){
     )/100;
 }
 
-
 class Particles{
     constructor(){
         this.x = Math.random() * canvas.width;
         this.y = 0;
         this.speed = 10;
-        this.velocity = Math.random() * 2.5;
+        this.velocity = Math.random() * 1.5;
         this.size = Math.random() * 1.5 + 1;
         this.position1 = Math.floor(this.y);
         this.position2 = Math.floor(this.x);
@@ -65,7 +63,7 @@ class Particles{
 
     draw(){
         ctx.beginPath();
-        ctx.fillStyle = 'green';
+        ctx.fillStyle = 'white';
         ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
         ctx.fill();
         }
@@ -92,3 +90,4 @@ class Particles{
     requestAnimationFrame(animate);
     })()
 });
+
